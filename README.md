630de196a064e3c070280429790eae44a85a6d29
gggmt <34018741+gmt710@users.noreply.github.com>
2022-03-18 17:35:30 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# this rep
+Simultaneous multi-threaded real-time reading, fetching and visualization of the camera
+
+# the chinese reference
+you can refer this : https://blog.csdn.net/qq_35975447/article/details/123548458
---
+++ camera_rg_thread_1.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+"""
+refer : https://blog.csdn.net/weixin_41576121/article/details/96128253
+Created on 2022/3/17 15:20
+@author  : GMT
+"""
+import cv2
+import threading
+from collections import deque
+import time
+
+
+class RealReadThread():
+    def __init__(self, input):
+        super(RealReadThread).__init__()
+        self._jobq = input
+        self.maxsizeq = input.maxlen
+        # ip_camera_url = 'rtsp://admin:admin@192.168.1.64/'   # rtsp数据流
+        # 创建一个窗口
+        self.cap = cv2.VideoCapture(0)
+        self.lockr = threading.Lock()
+        self.thread = threading.Thread(target=self.update, args=([self.cap]), daemon=True)
+        self.thread.start()
+
+    def update(self, cap):
+        # cv2.namedWindow('ip_camera', flags=cv2.WINDOW_NORMAL | cv2.WINDOW_FREERATIO)
+        if not cap.isOpened():
+            print('请检查IP地址还有端口号，或者查看IP摄像头是否开启，另外记得使用sudo权限运行脚本')
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                continue
+            self.lockr.acquire()
+            if len(self._jobq) == self.maxsizeq:
+                self._jobq.popleft()
+            else:
+                self._jobq.append(frame)
+            self.lockr.release()
+            # cv2.imshow('ip_camera', frame)
+            #             # if cv2.waitKey(10) == ord('q'):
+            #             #     # 退出程序
+            #             #     break
+            time.sleep(0.001)
+        print("实时读取线程退出！！！！")
+        # cv2.destroyWindow('ip_camera')
+        self._jobq.clear()  # 读取进程结束时清空队列
+        self.cap.release()
+
+
+class GetThread():
+    def __init__(self, input, result):
+        super(GetThread).__init__()
+        self._jobq = input
+        self.maxsizeq = input.maxlen
+        self.frame_idx = 0
+        self.callbacks = []
+        self.result = result
+        self.lockg = threading.Lock()
+        self.thread = threading.Thread(target=self.update, daemon=True)
+        self.thread.start()
+
+    def addcallback(self, callback):
+        self.callbacks.append(callback)
+
+    def update(self):
+        # cv2.namedWindow('get', flags=cv2.WINDOW_NORMAL | cv2.WINDOW_FREERATIO)
+        flag = False
+        while (True):
+            try:
+                if len(self._jobq) != 0:
+                    self.lockg.acquire()
+                    im_new = self._jobq.popleft()
+                    if im_new is not None:
+                        self.frame_idx += 1
+
+                    if len(self.result) == self.maxsizeq:
+                        self.result.popleft()
+                    else:
+                        self.result.append((self.frame_idx, im_new))
+                    for cb in self.callbacks:
+                        cb(self.result)
+                    self.lockg.release()
+
+                    flag = True
+
+                elif flag == True and len(self._jobq) == 0:
+                    time.sleep(0.001)
+                    # break
+            except Exception as e:
+                print(e)
+        print("间隔1s获取图像线程退出！！！！")
+
+    def get_result(self, result):
+        return result
+
+
+
+if __name__ == "__main__":
+    maxsizeq = 10
+    q = deque([], maxsizeq)
+    result = deque([], 10)
+    th1 = RealReadThread(q)
+    th2 = GetThread(q, result)
+    ths = [th1, th2]
+    print("--------------begin---------------")
+    for i, th in enumerate(ths):
+        if i == 1:
+            th.addcallback(th.get_result)
+
+    cv2.namedWindow('img_show', flags=cv2.WINDOW_NORMAL | cv2.WINDOW_FREERATIO)
+    cv2.startWindowThread()
+    frame_idx = last_frame_idx = 0
+
+    while True:
+        try:
+            if len(result) != 0:
+                frame_idx, your_img = result.popleft()
+                if frame_idx == last_frame_idx:
+                    continue
+                print("now you can process your img index : {}".format(frame_idx))
+                last_frame_idx = frame_idx
+                cv2.imshow("img_show", your_img)
+                cv2.waitKey(1)
+        except Exception as e:
+            print("-------------------------", e)
+            continue
